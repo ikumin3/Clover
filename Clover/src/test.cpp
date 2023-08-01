5342934f893aa9fc86a1367324987c64ad8808db
ikumin3 <primeclover193@gmail.com>
2023-08-02 07:08:27 +0900

    Setup CloverEngine Project

---
+++ Clover/src/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+#include <stdio.h>
+
+namespace Clover {
+
+	void Print() {
+		printf("Hello CloverEngine");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,9 @@
+namespace Clover {
+
+	__declspec(dllimport) void Print();
+}
+
+
+void main() {
+	Clover::Print();
+}
