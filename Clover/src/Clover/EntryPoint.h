7cbb136acfba0c4d7057703a689202d0aa8b25fd
ikumin3 <primeclover193@gmail.com>
2023-08-04 01:53:40 +0900

    保存忘れ

--- Clover/src/Clover/EntryPoint.h
+++ Clover/src/Clover/EntryPoint.h
@@ -6,7 +6,6 @@ extern Clover::Application* Clover::CreateApplication();
 
 int main(int argc,char** argv)
 {
-	printf("Clover Engine");
 	auto app = Clover::CreateApplication();
 	app->Run();
 	delete app;
